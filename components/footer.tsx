4586655e86ed6518b8bac1d16bc5aaad94122573
Cliff Cunnington <cliff@10strata.com>
2023-11-07 12:41:09 +0100

    Add the dependencies and hooks for full functionality

---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="text-center mt-28 ">
+      <hr />
+      <h5 className="text-2xl font-semibold">Copyright now</h5>
+    </footer>
+  );
+}
