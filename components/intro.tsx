914f40de25a9e3c686b120240a15e797d415d084
Cliff Cunnington <cliff@10strata.com>
2023-11-08 07:31:18 +0100

    BUGFIX: z-values for header and intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -18,7 +18,7 @@ export default function Intro() {
   return (
     <section
       id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem] z-10"
     >
       <div className="flex items-center justify-center">
         <div className="relative">
